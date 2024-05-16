b9f7902ea49233046c28cfdab99687890fa51c9f
alielmouedden <alielmouedden9@gmail.com>
2024-05-16 12:47:43 +0100

    Update menu-items.ts

--- src/app/shared/sidebar/menu-items.ts
+++ src/app/shared/sidebar/menu-items.ts
@@ -34,14 +34,6 @@ export const ROUTES: RouteInfo[] = [
     extralink: false,
     submenu: []
   },
-  // {
-  //   path: '/component/For.continu',
-  //   title: 'For.continu',
-  //   icon: 'bi bi-card-text',
-  //   class: '',
-  //   extralink: false,
-  //   submenu: []
-  // },
   {
     path: '/component/Affectation',
     title: 'Affectation',
@@ -50,36 +42,36 @@ export const ROUTES: RouteInfo[] = [
     extralink: false,
     submenu: []
   },
-  // {
-  //   path: '/component/Fiche.présence',
-  //   title: 'Fiche présence',
-  //   icon: 'bi bi-dice-1',
-  //   class: '',
-  //   extralink: false,
-  //   submenu: []
-  // },
-  // {
-  //   path: '/component/Evaluation',
-  //   title: 'Evaluation',
-  //   icon: 'bi bi-pause-btn',
-  //   class: '',
-  //   extralink: false,
-  //   submenu: []
-  // },
-  // {
-  //   path: '/component/Evaluation.For',
-  //   title: 'Evaluation.For',
-  //   icon: 'bi bi-pause-btn',
-  //   class: '',
-  //   extralink: false,
-  //   submenu: []
-  // },
-  // {
-  //   path: '/component/Bilan',
-  //   title: 'Bilan',
-  //   icon: 'bi bi-layout-split',
-  //   class: '',
-  //   extralink: false,
-  //   submenu: []
-  // },
+  {
+    path: '/component/Fiche.présence',
+    title: 'Fiche présence',
+    icon: 'bi bi-dice-1',
+    class: '',
+    extralink: false,
+    submenu: []
+  },
+  {
+    path: '/component/Evaluation',
+    title: 'Evaluation',
+    icon: 'bi bi-pause-btn',
+    class: '',
+    extralink: false,
+    submenu: []
+  },
+  {
+    path: '/component/Evaluation.For',
+    title: 'Evaluation.For',
+    icon: 'bi bi-pause-btn',
+    class: '',
+    extralink: false,
+    submenu: []
+  },
+  {
+    path: '/component/Bilan',
+    title: 'Bilan',
+    icon: 'bi bi-layout-split',
+    class: '',
+    extralink: false,
+    submenu: []
+  },
 ];
